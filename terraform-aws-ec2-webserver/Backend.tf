863a62610e123619c7f98c2eb28684286996a931
Rizwaan Rahat <7895mrrizwan@gmail.com>
2026-05-01 04:58:22 +0530

    Delete terraform-aws-ec2-webserver directory

--- terraform-aws-ec2-webserver/Backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "terraformtfstate129"
-    key    = "Terraform/Exercise6/terraform.tfstate"
-    region = "us-east-1"
-  }
-}
--- terraform-aws-ec2-webserver/Scripts/save_output.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "Instance ID: $1" > instance-info.txt
-echo "Public IP: $2" >> instance-info.txt
-echo "Private IP: $3" >> instance-info.txt
